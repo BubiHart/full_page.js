e04f45b0d7a38887e5b5403a3b99f5c1c6ac2737
BubiHart <piskovyi.dmytro@gmail.com>
2018-02-06 14:39:54 +0200

    add jquery and fullpage.js plugins

--- index.php
+++ index.php
@@ -3,9 +3,12 @@
   <head>
     <meta charset="utf-8">
     <title>Full page</title>
+    <script src="libs/jquery/jquery.js" charset="utf-8"></script>
     <link rel="stylesheet" href="libs/normalize/normalize.css">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="libs/full_page/jquery.fullPage.css">
     <script src="js/main.js" charset="utf-8"></script>
+    <script src="libs/full_page/jquery.fullpage.js" charset="utf-8"></script>
   </head>
   <body>
 
