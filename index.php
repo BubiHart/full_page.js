a08d27c9c1fcdcc845b0daa0f805344d128664be
BubiHart <piskovyi.dmytro@gmail.com>
2018-02-06 14:24:48 +0200

    add basic files to project

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Full page</title>
+    <link rel="stylesheet" href="libs/normalize/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/main.js" charset="utf-8"></script>
+  </head>
+  <body>
+
+
+  </body>
+</html>
